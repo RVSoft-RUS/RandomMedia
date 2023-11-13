1e6b42bc48502dbb793b8daaade0b3debe0b482e
RVSoft-RUS <rovs15ds@gmail.com>
2023-11-13 23:20:39 +0300

    +

---
+++ src/main/java/ru/rvsoft/randommedia/RandomMediaApplication.java
@@ -0,0 +1,13 @@
+package ru.rvsoft.randommedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomMediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomMediaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/rvsoft/randommedia/RandomMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rvsoft.randommedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomMediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
